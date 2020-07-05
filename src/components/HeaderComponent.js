f4c42f5637914b30f6e9f10229a201a19717cac9
Nitin Sharma <nitin.g.sharma@capgemini.com>
2020-07-05 14:07:21 +0530

    Redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -5,13 +5,17 @@ import {
     Form, FormGroup, Input, Label
 } from 'reactstrap'; import { NavLink } from 'react-router-dom';
 import '../../src/App.css'
+
 class Header extends Component {
+
     constructor(props) {
         super(props);
+
         this.state = {
             isNavOpen: false,
             isModalOpen: false
         };
+
         this.toggleNav = this.toggleNav.bind(this);
         this.toggleModal = this.toggleModal.bind(this);
         this.handleLogin = this.handleLogin.bind(this);
@@ -80,6 +84,7 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+                
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
                     <ModalBody>
